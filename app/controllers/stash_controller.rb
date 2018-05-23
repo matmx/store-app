e190a1d37862e95666697af45ae586d632a67ec6
Matt Teague <mteague2014@gmail.com>
2018-05-23 13:27:34 +0000

    Define instance variables in Todo show action, and place in show view

--- app/controllers/stash_controller.rb
+++ app/controllers/stash_controller.rb
@@ -4,4 +4,8 @@ def home
     
 end
 
+def show
+  @food_item = "Flamin Hots"
+  @dollar_amount = 1.00
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 
 get 'store', to: 'stash#home'
-
+get 'show', to: 'stash#show'
 end
